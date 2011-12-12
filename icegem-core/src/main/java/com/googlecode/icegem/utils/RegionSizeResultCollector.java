44c01d9bb4c490aa0e4c3a2ddba762366040e5c3
Alexey Kharlamov <aharlamov@gmail.com>
2011-12-13 01:11:35 +0400

    Fixes #36. Removed excessive synchronization in RegionSizeResultCollector.

--- icegem-core/src/main/java/com/googlecode/icegem/utils/RegionSizeResultCollector.java
+++ icegem-core/src/main/java/com/googlecode/icegem/utils/RegionSizeResultCollector.java
@@ -35,58 +35,35 @@ import com.gemstone.gemfire.cache.execute.ResultCollector;
 import com.gemstone.gemfire.distributed.DistributedMember;
 
 /**
+ * is a summing collector for region size call.
+ * 
  * @author Renat Akhmerov.
  */
-public class RegionSizeResultCollector implements ResultCollector<Integer, Integer> {
-    /** Collected region size. */
-    private int size;
+public class RegionSizeResultCollector implements
+		ResultCollector<Integer, Integer> {
+	/** Collected region size. */
+	private int size;
 
-    /** Mutex. */
-    private final Object mux = new Object();
-
-    /** Done flag. */
-    private boolean done;
-
-    public void addResult(DistributedMember memberID, Integer singleRes) {
-	if (singleRes != null)
-	    size += singleRes;
-    }
-
-    public void clearResults() {
-	size = 0;
-    }
-
-    public void endResults() {
-	synchronized (mux) {
-	    done = true;
-
-	    mux.notifyAll();
-	}
-    }
-
-    public Integer getResult() throws FunctionException {
-	try {
-	    return getResult(0, null);
-	} catch (InterruptedException e) {
-	    // Should never happen.
-	    throw new FunctionException(e);
-	}
-    }
-
-    public Integer getResult(long timeout, TimeUnit unit) throws FunctionException, InterruptedException {
-	if (timeout > 0 && unit == null)
-	    throw new IllegalArgumentException("Parameter unit cannot be nul if timeout > 0");
-
-	synchronized (mux) {
-	    while (!done) {
-		if (timeout > 0) {
-		    unit.timedWait(mux, timeout);
-		} else {
-		    mux.wait();
+	public synchronized void addResult(DistributedMember memberID,
+			Integer singleRes) {
+		if (singleRes != null) {
+			size += singleRes;
 		}
-	    }
 	}
 
-	return size;
-    }
+	public void clearResults() {
+		size = 0;
+	}
+
+	public void endResults() {
+		// Nothing to do.
+	}
+
+	public Integer getResult() throws FunctionException {
+		return getResult(0, null);
+	}
+
+	public Integer getResult(long timeout, TimeUnit unit) {
+		return size;
+	}
 }
